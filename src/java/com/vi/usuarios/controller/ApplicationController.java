56a180801697528cfffd3c6e3ce203ad8452b36b
jerviver21 <jerviver21@localhost>
2013-08-28 18:06:42 -0500

    Organizacion del código y documentación

--- src/java/com/vi/usuarios/controller/ApplicationController.java
+++ src/java/com/vi/usuarios/controller/ApplicationController.java
@@ -3,6 +3,9 @@ package com.vi.usuarios.controller;
 
 import com.vi.comun.locator.ParameterLocator;
 import com.vi.comun.services.CommonServicesLocal;
+import com.vi.util.FacesUtil;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import javax.annotation.PostConstruct;
@@ -10,6 +13,7 @@ import javax.ejb.EJB;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.event.ValueChangeEvent;
+import javax.faces.model.SelectItem;
 
 /**
  * @author Jerson Viveros
@@ -23,19 +27,29 @@ public class ApplicationController {
     private String ROL_MASTER;
     
     //Define la plantilla y layout de la aplicacion
-
-    private String p1 = "../plantilla1.xhtml";
-    private String p2 = "../plantilla2.xhtml";
-    private String p3 = "../plantilla3.xhtml";
-    private String plantilla = p3;
-    private String plantillaIndex = p3;
-    private Map<String, String> themes; 
-    private String theme = "bluesky";
+    private String estiloA1 = "estilo1A1.css";
+    private String estiloA2 = "estilo3A2.css";
+    private String estiloB1 = "estilo3B1.css";
+    private String estiloB2 = "estilo1B2.css";
+    private String estiloC1 = "estilo1A1.css";
+    private String plan1 = "plantillaA1.xhtml";
+    private String plan2 = "plantillaB2.xhtml";
+    private String plan3 = "plantillaB1.xhtml";
+    private String theme = "casablanca";
     private String url;
+    
+    
+    
+    
+    private Map<String, String> themes; 
+    private List<SelectItem> plantillas;
+    private List<SelectItem> estilos;
+    
     
 
     @EJB
     private CommonServicesLocal commonServices;
+
     
 
     @PostConstruct
@@ -44,7 +58,7 @@ public class ApplicationController {
         System.out.println("Iniciando la aplicación MH System...");
         commonServices.updateEstructuraMenus();
         System.out.println("Menus actualizados...");
-        //licenciaServices.initTimer();
+        //clasificadoTimer.initTimer();
         //System.out.println("Timer iniciado...");
         url = locator.getParameter("url");
 
@@ -60,11 +74,21 @@ public class ApplicationController {
         themes.put("Cupertino", "cupertino"); 
         themes.put("Redmond", "redmond"); 
         themes.put("Glass-X", "glass-x"); 
+        
+        
+        Map mPlans = new HashMap();
+        mPlans.put("plantillaA1.xhtml", "Plantilla A1");
+        mPlans.put("plantillaA2.xhtml", "Plantilla A2");
+        mPlans.put("plantillaB1.xhtml", "Plantilla B1");
+        mPlans.put("plantillaC1.xhtml", "Plantilla C1");
+        plantillas = FacesUtil.getSelectsItem(mPlans);
+        
+       
+        
     }
     
     
     public void cambiarTema(ValueChangeEvent event){
-        System.out.println("Nuevo tema: "+event.getNewValue());
         theme = (String)event.getNewValue();
     }
     
@@ -84,49 +108,7 @@ public class ApplicationController {
         return ROL_MASTER;
     }   
 
-    /**
-     * @return the plantilla
-     */
-    public String getPlantilla() {
-        return plantilla;
-    }
-    
-    
-
-    /**
-     * @param plantilla the plantilla to set
-     */
-    public void setPlantilla(String plantilla) {
-        this.plantilla = plantilla;
-    }
-
-    /**
-     * @return the p1
-     */
-    public String getP1() {
-        return p1;
-    }
-
-    /**
-     * @param p1 the p1 to set
-     */
-    public void setP1(String p1) {
-        this.p1 = p1;
-    }
-
-    /**
-     * @return the p2
-     */
-    public String getP2() {
-        return p2;
-    }
-
-    /**
-     * @param p2 the p2 to set
-     */
-    public void setP2(String p2) {
-        this.p2 = p2;
-    }
+   
 
 
 
@@ -158,16 +140,6 @@ public class ApplicationController {
         this.theme = theme;
     }
 
-    /**
-     * @param plantillaIndex the plantillaIndex to set
-     */
-    public void setPlantillaIndex(String plantillaIndex) {
-        this.plantillaIndex = plantillaIndex;
-    }
-    
-    public String getPlantillaIndex() {
-        return plantilla.replaceAll("(.*plantilla)(\\d.xhtml)", "$1I$2");
-    }
 
     /**
      * @return the url
@@ -182,5 +154,151 @@ public class ApplicationController {
     public void setUrl(String url) {
         this.url = url;
     }
+
+    
+
+    /**
+     * @return the plan1
+     */
+    public String getPlan1() {
+        return plan1;
+    }
+
+    /**
+     * @param plan1 the plan1 to set
+     */
+    public void setPlan1(String plan1) {
+        this.plan1 = plan1;
+    }
+
+    /**
+     * @return the plan2
+     */
+    public String getPlan2() {
+        return plan2;
+    }
+
+    /**
+     * @param plan2 the plan2 to set
+     */
+    public void setPlan2(String plan2) {
+        this.plan2 = plan2;
+    }
+
+    /**
+     * @return the plan3
+     */
+    public String getPlan3() {
+        return plan3;
+    }
+
+    /**
+     * @param plan3 the plan3 to set
+     */
+    public void setPlan3(String plan3) {
+        this.plan3 = plan3;
+    }
+
+    /**
+     * @return the plantillas
+     */
+    public List<SelectItem> getPlantillas() {
+        return plantillas;
+    }
+
+    /**
+     * @param plantillas the plantillas to set
+     */
+    public void setPlantillas(List<SelectItem> plantillas) {
+        this.plantillas = plantillas;
+    }
+
+    /**
+     * @return the estilos
+     */
+    public List<SelectItem> getEstilos() {
+        return estilos;
+    }
+
+    /**
+     * @param estilos the estilos to set
+     */
+    public void setEstilos(List<SelectItem> estilos) {
+        this.estilos = estilos;
+    }
+
+    /**
+     * @return the estiloA1
+     */
+    public String getEstiloA1() {
+        return estiloA1;
+    }
+
+    /**
+     * @param estiloA1 the estiloA1 to set
+     */
+    public void setEstiloA1(String estiloA1) {
+        this.estiloA1 = estiloA1;
+    }
+
+    /**
+     * @return the estiloA2
+     */
+    public String getEstiloA2() {
+        return estiloA2;
+    }
+
+    /**
+     * @param estiloA2 the estiloA2 to set
+     */
+    public void setEstiloA2(String estiloA2) {
+        this.estiloA2 = estiloA2;
+    }
+
+    /**
+     * @return the estiloB1
+     */
+    public String getEstiloB1() {
+        return estiloB1;
+    }
+
+    /**
+     * @param estiloB1 the estiloB1 to set
+     */
+    public void setEstiloB1(String estiloB1) {
+        this.estiloB1 = estiloB1;
+    }
+
+    /**
+     * @return the estiloC1
+     */
+    public String getEstiloC1() {
+        return estiloC1;
+    }
+
+    /**
+     * @param estiloC1 the estiloC1 to set
+     */
+    public void setEstiloC1(String estiloC1) {
+        this.estiloC1 = estiloC1;
+    }
+
+    /**
+     * @return the estiloB2
+     */
+    public String getEstiloB2() {
+        return estiloB2;
+    }
+
+    /**
+     * @param estiloB2 the estiloB2 to set
+     */
+    public void setEstiloB2(String estiloB2) {
+        this.estiloB2 = estiloB2;
+    }
+
+
+
+    
 
 }
